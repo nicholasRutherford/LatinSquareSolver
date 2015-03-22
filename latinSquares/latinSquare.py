67540f8701b193645f87618aabbf67b8687fd5b6
Nicholas Rutherford <zgithub@isomorphism.ca>
2015-03-21 18:40:04 -0600

    Add holes to latin squares

--- latinSquares/latinSquare.py
+++ latinSquares/latinSquare.py
@@ -18,6 +18,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import random
 
+class Hole:
+
+    def __init__(self):
+        self.verified = False
+        self.options = []
+        self.value = -1
+
+    def __str__(self):
+        if self.value == -1:
+            return " "
+        else:
+            return str(self.value)
+
+
 class LatinSquare:
 
     def randomise(self):
@@ -37,12 +51,23 @@ class LatinSquare:
                 self.grid[i][x] = self.grid[i][swapCol]
                 self.grid[i][swapCol] = temp
 
+    def addHoles(self, k):
+        possibleOptions = range(self.n ** 2)
+        selections = random.sample(possibleOptions, k)
+
+        for opt in selections:
+            print opt
+            x = opt / self.n
+            y = opt % self.n
+            self.grid[x][y] = Hole()
+
+
     def __init__(self, n, k, seed=1337):
         random.seed(seed)
         self.n = n
         self.grid = [[(x+y)% n for x in range(n)] for y in range(n)]
         self.randomise()
-        self.k = k
+        self.addHoles(k)
 
     def __str__(self):
         output = ""
@@ -53,5 +78,5 @@ class LatinSquare:
         return output[:-1] #Ignore last newline
 
 if __name__ == "__main__":
-    sq = LatinSquare(5, 5)
+    sq = LatinSquare(5, 5, seed=1337)
     print sq
